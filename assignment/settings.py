b259aa3b5ae691f050f899fc2ccab513e42c9003
Dwaipayan Barman <dwaipayanb1994@gmail.com>
2019-04-20 15:49:44 +0530

    allowed host

--- assignment/settings.py
+++ assignment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hjr=s5!@88*u3qd^^&s&^1s-k2au@1h$dq!ss7tojwas&+2s#2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
