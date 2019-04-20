8dfcc4424a43e80964fbd77a041f568481715682
Dwaipayan Barman <dwaipayanb1994@gmail.com>
2019-04-20 16:30:29 +0530

    Update settings.py

--- assignment/settings.py
+++ assignment/settings.py
@@ -14,6 +14,8 @@ import os
 
 import dj_database_url
 
+db_from_env = dj_database_url.config()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -142,4 +144,4 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
