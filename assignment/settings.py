0f732100ad5cfbd2e872d002d540178753a1b4af
Dwaipayan Barman <dwaipayanb1994@gmail.com>
2019-04-20 15:48:23 +0530

    debug true for 400

--- assignment/settings.py
+++ assignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hjr=s5!@88*u3qd^^&s&^1s-k2au@1h$dq!ss7tojwas&+2s#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
