e76b7ef67b747819508bf5281e1610c8579dfa24
Dwaipayan Barman <dwaipayanb1994@gmail.com>
2019-04-20 16:20:09 +0530

    Update settings.py

--- assignment/settings.py
+++ assignment/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,3 +141,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
